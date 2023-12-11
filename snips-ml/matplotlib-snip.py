423031a1ee7f5bb9fb083cfef8fa7801ccedf504
Jennifer E. Yoon <jenneyoon@gmail.com>
2023-12-11 00:49:33 -0500

    Update matplotlib-snip.py

--- snips-ml/matplotlib-snip.py
+++ snips-ml/matplotlib-snip.py
@@ -6,3 +6,17 @@ import matplotlib.pyplot as plt
 plt.plot([1, 2, 4, 9, 5, 3])
 plt.show()
 
+import numpy as np  
+
+# color & tick marks shortcut   
+x = np.linspace(-1.4, 1.4, 30)
+plt.plot(x, x, 'g--', x, x**2, 'r:', x, x**3, 'b^')
+plt.show()
+
+### subplots function, fig, ax explicit  
+
+
+
+### pylab implicit figure, gcf()  
+
+

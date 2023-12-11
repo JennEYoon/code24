f9026ddd2aa43a47c7b14e0a7dee82de79c86922
Jennifer E. Yoon <jenneyoon@gmail.com>
2023-12-11 02:15:56 -0500

    Update matplotlib-snip.py

--- snips-ml/matplotlib-snip.py
+++ snips-ml/matplotlib-snip.py
@@ -17,6 +17,24 @@ plt.show()
 
 
 
-### pylab implicit figure, gcf()  
+### pylab implicit figure, gcf(), plt.subplot(row, col, index)   
+"""
+Subplots
+A matplotlib figure may contain multiple subplots. 
+These subplots are organized in a grid. 
+To create a subplot, call the * subplot * function, and specify the number of rows and columns
+in the figure, and the index of the subplot you want to draw on (starting from 1, 
+then left to right, and top to bottom). 
+Note that pyplot keeps track of the currently active subplot 
+(which you can get a reference to by calling plt.gca()), so when you call the plot function, 
+it draws on the active subplot.
+"""
 
+x = np.linspace(-1.4, 1.4, 30)
+plt.subplot(2, 2, 1)  # 2 rows, 2 columns, 1st subplot = top left
+plt.plot(x, x)
+plt.subplot(2, 2, 2)  # 2 rows, 2 columns, 2nd subplot = top right
+plt.plot(x, x**2)
+plt.subplot(2, 2, 3)  # 2 rows, 2 columns, 3rd subplot = bottom left
+plt.plot(x, x**3)
 

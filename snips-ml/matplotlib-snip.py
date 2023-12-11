50fd49e64629465be589ea2a89a5194177ac07f8
Jennifer E. Yoon <jenneyoon@gmail.com>
2023-12-11 02:17:14 -0500

    Update matplotlib-snip.py

--- snips-ml/matplotlib-snip.py
+++ snips-ml/matplotlib-snip.py
@@ -37,4 +37,8 @@ plt.subplot(2, 2, 2)  # 2 rows, 2 columns, 2nd subplot = top right
 plt.plot(x, x**2)
 plt.subplot(2, 2, 3)  # 2 rows, 2 columns, 3rd subplot = bottom left
 plt.plot(x, x**3)
+plt.subplot(2, 2, 4)  # 2 rows, 2 columns, 4th subplot = bottom right
+plt.plot(x, x**4)
+plt.show()
+
 

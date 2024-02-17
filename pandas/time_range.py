f4935921ea95055aca5b17b77e2eb13861f47dfd
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-02-16 23:54:03 -0500

    Update time_range.py

--- pandas/time_range.py
+++ pandas/time_range.py
@@ -5,4 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt  
 
 dates = pd.date_range('2016/10/29 5:30pm', periods=12, freq='H')
-dates
+print(dates)  
+
+
+temperatures = [4.4,5.1,6.1,6.2,6.1,6.1,5.7,5.2,4.7,4.1,3.9,3.5]
+s7 = pd.Series(temperatures, name="Temperature")
+s7.plot()
+plt.show()

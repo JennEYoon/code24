07e70a1786c8c5cfebb3c94a8f9eb5086f26b80f
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-02-16 23:56:56 -0500

    Update time_range.py

--- pandas/time_range.py
+++ pandas/time_range.py
@@ -5,10 +5,18 @@ import pandas as pd
 import matplotlib.pyplot as plt  
 
 dates = pd.date_range('2016/10/29 5:30pm', periods=12, freq='H')
+# 12 hours
 print(dates)  
 
 
 temperatures = [4.4,5.1,6.1,6.2,6.1,6.1,5.7,5.2,4.7,4.1,3.9,3.5]
+# 12 data points
 s7 = pd.Series(temperatures, name="Temperature")
 s7.plot()
+plt.show()
+
+
+temp_series.plot(kind="bar")
+# plot temperature using hours.  
+plt.grid(True)
 plt.show()

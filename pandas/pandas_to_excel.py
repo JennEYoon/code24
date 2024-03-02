1fec34977812d7fd9fd07cc07a804b95d1d4adc1
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-03-02 00:25:46 -0500

    Update pandas_to_excel.py

--- pandas/pandas_to_excel.py
+++ pandas/pandas_to_excel.py
@@ -3,10 +3,20 @@
 import pandas as pd  
 import ExcelWriter as writer  
 
-df1 =  
-df2 =  
+data = {('employees': 'Mary', 'Hank', 'Jennifer', 'Joo-In'), ('hire_date': '2000', '1998', '1992', '2012')}
+df1 = pd.DataFrame(data)
+
+print(df.head())
+file ='/contents/salary.xls.sheet1')
+df1.to_excel(writer, file)
+
+
+salary = {'Mary': 102, 'Joo-In: 56, 'Hank: '115', 'Jennifer': 220}
+df2 = pd.DataFrame(salary, colnames=('salary')).T  
+
+df3 = pd.merge(df1, df2)  
 
 # Save df1 to Sheet1, df2 to Sheet2  
 
-file = writer.
+
 

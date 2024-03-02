b1be9841a05609807f3a082d328dcf154c8fadf6
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-03-01 23:27:43 -0500

    Create pandas_to_excel.py

---
+++ pandas/pandas_to_excel.py
@@ -0,0 +1,12 @@
+# Practice, Pandas and Excel file format   
+
+import pandas as pd  
+import ExcelWriter as writer  
+
+df1 =  
+df2 =  
+
+# Save df1 to Sheet1, df2 to Sheet2  
+
+file = writer.
+

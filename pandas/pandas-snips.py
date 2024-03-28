981e7de1ba89ec9cebe05045c7f0e07e730a0b60
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-03-28 00:09:07 -0400

    Update pandas-snips.py

--- pandas/pandas-snips.py
+++ pandas/pandas-snips.py
@@ -9,5 +9,13 @@ df = pd.read_csv('file1.csv')
 print(df.head())
 
 df.to_csv('file2.csv')
-df.to_excel('file.xlsx', sheet_name='Sheet1')  
+df.to_excel('file.xlsx', sheet_name='Sheet1')   
+
+# or import ExcelWriter, context window with open   
+import ExcelWriter   
+
+with f as ExcelWriter('myexcel.xlsx'):  
+    df.to_excel(f, sheet_name="sheet1")  
+    df2.to_excel(f, sheet_name='sheet2")   
+
 

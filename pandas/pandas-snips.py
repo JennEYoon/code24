2f43837e6a85fc73a8deb561270ef6655a2bd875
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-03-27 23:48:51 -0400

    Update pandas-snips.py

--- pandas/pandas-snips.py
+++ pandas/pandas-snips.py
@@ -1,7 +1,13 @@
-import pandas as pd  
+import matplotlib  
+import matplotlib.pyplot as plt  
 import numpy as np
+import pandas as pd  
+from pandas import Series, DataFrame  
+import seaborn as sns  
 
 df = pd.read_csv('file1.csv')  
 print(df.head())
 
 df.to_csv('file2.csv')
+df.to_excel('file.xls', sheetname='Sheet1')  
+

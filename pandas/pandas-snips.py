dd265d9d9adff4377b86246002f79d867a6679e3
Jennifer E. Yoon <jenneyoon@gmail.com>
2024-03-28 00:13:46 -0400

    Update pandas-snips.py

--- pandas/pandas-snips.py
+++ pandas/pandas-snips.py
@@ -2,7 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt  
 import numpy as np
 import pandas as pd  
-from pandas import Series, DataFrame  
+from pandas import Series, DataFrame, ExcelWriter     
 import seaborn as sns  
 
 df = pd.read_csv('file1.csv')  
@@ -12,10 +12,11 @@ df.to_csv('file2.csv')
 df.to_excel('file.xlsx', sheet_name='Sheet1')   
 
 # or import ExcelWriter, context window with open   
-import ExcelWriter   
+from pandas import ExcelWriter   
+# worked on Colab without importing ExcelWriter   
 
-with f as ExcelWriter('myexcel.xlsx'):  
-    df.to_excel(f, sheet_name="sheet1")  
+with ExcelWriter('myexcel.xlsx') as f:   
+    df1.to_excel(f, sheet_name="sheet1")  
     df2.to_excel(f, sheet_name='sheet2")   
 
 
